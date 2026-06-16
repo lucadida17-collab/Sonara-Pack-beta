2b41568a610145cdef4b1bf2dd18b7f22e7e6bb0
Luca Dida <luca.dida17@gmail.com>
2026-06-16 14:38:30 +0200

    add r2 artist image upload

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads", { recursive: true });
+}
+
 app.use("/uploads", express.static("uploads"));
 
 const downloadsPath = path.join(__dirname, "downloads");
